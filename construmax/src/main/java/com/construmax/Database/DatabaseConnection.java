1f00968ba5555b0e2b7d3087c417f79c6c216def
Luiz Matheus <luizmatheus@MacBook-Air-de-Luiz.local>
2025-10-31 20:45:27 -0300

    Conexão com o banco e dependencias

---
+++ construmax/src/main/java/com/construmax/Database/DatabaseConnection.java
@@ -0,0 +1,5 @@
+package com.construmax.Database;
+
+public class DatabaseConnection {
+    
+}
--- construmax/src/main/java/module-info.java
+++ construmax/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.construmax {
     requires transitive javafx.graphics;
     requires javafx.base;
     requires javafx.fxml;
+    requires java.sql;
     opens com.construmax.Controllers to javafx.fxml;
     opens com.construmax to javafx.fxml;
     exports com.construmax;
