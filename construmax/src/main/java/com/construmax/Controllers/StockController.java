56fe476e7bba810fc98cd2a2b731d29c79e18256
João Vitor Fernandes de Sousa <joaovitorf1611@gmail.com>
2025-11-04 11:05:45 -0300

    campus de tabela funcionando

--- construmax/src/main/java/com/construmax/Controllers/StockController.java
+++ construmax/src/main/java/com/construmax/Controllers/StockController.java
@@ -18,9 +18,17 @@ public class StockController {
   @FXML
   private TableColumn<Equipment, String> type;
   @FXML
+  private TableColumn<Equipment, String> description;
+  @FXML
+  private TableColumn<Equipment, Status> status;
+  @FXML
+  private TableColumn<Equipment, Double> dailyValue;
   public void initialize () {
     name.setCellValueFactory(new PropertyValueFactory<>("name"));
     type.setCellValueFactory(new PropertyValueFactory<>("type"));
+    description.setCellValueFactory(new PropertyValueFactory<>("description"));
+    status.setCellValueFactory(new PropertyValueFactory<>("status"));
+    dailyValue.setCellValueFactory(new PropertyValueFactory<>("dailyValue"));
     ObservableList<Equipment> equipments = FXCollections.observableArrayList(
       new Equipment("Chave de fenda", "Ferramenta", "Vonder Imantada Magnética", Status.AVAILABLE, 5.0)
     );
